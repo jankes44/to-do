f3b26b0b68a86db77a4a1e1a42fe7df0b82145ce
jackie44 <kalima12345@gmail.com>
2020-02-12 13:19:55 +0000

    Updates

---
+++ precache-manifest.8fb9a054cd46abfe537121f7eede05ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0614217133d9d5e15e984277d31d1f9",
+    "url": "/to-do/index.html"
+  },
+  {
+    "revision": "17e8b4c250ce1e8f1617",
+    "url": "/to-do/static/css/main.bf9ac4ce.chunk.css"
+  },
+  {
+    "revision": "0a2387e6fd837a0353ba",
+    "url": "/to-do/static/js/2.e60e1379.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/to-do/static/js/2.e60e1379.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17e8b4c250ce1e8f1617",
+    "url": "/to-do/static/js/main.090429e0.chunk.js"
+  },
+  {
+    "revision": "8b9f7010519e44a820b7",
+    "url": "/to-do/static/js/runtime-main.0ea6de14.js"
+  }
+]);
